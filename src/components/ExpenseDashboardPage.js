a9666e36457bb5bb411363d5b41236bc8be25fd9
Karolis Zigas <karolisbzigas@gmail.com>
2023-04-06 16:28:31 +0300

    Included summary

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ExpenseList from './ExpenseList';
 import { connect } from 'react-redux';
 import ExpenseListFilters from './ExpenseListFilters';
+import ExpensesSummary from './ExpensesSummary';
 
 const ExpenseDashboardPage = () => {
     return (
     <div>
+        <ExpensesSummary />
         <ExpenseListFilters />
         <ExpenseList />
     </div>
---
+++ src/firebase/firebase.js
@@ -0,0 +1,12 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAj23wICy1Ru2q_LHsI4EfBeHmYVZokiNk",
+    authDomain: "budget-241d9.firebaseapp.com",
+    databaseURL: "https://budget-241d9-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "budget-241d9",
+    storageBucket: "budget-241d9.appspot.com",
+    messagingSenderId: "804841756379",
+    appId: "1:804841756379:web:c7bc0acdc84bf7d90a1efe",
+    measurementId: "G-PHQX833K46"
+  };
+
